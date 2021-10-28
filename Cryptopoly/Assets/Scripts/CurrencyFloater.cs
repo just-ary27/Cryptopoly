7c6311a48ed934ebafb8594fa3f54cd67d61a74e
just-ary27 <aryan_r@ch.iitr.ac.in>
2021-10-28 16:42:45 +0530

    Added floating animation

--- Cryptopoly/Assets/Scripts/CurrencyFloater.cs
+++ Cryptopoly/Assets/Scripts/CurrencyFloater.cs
@@ -4,17 +4,25 @@ public class CurrencyFloater : MonoBehaviour
 {
     private float initY;
     private float initX;
+    private Vector3 posOffset = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
-        float initY = transform.position.y;
-        float initX = transform.position.x;
+        initY = transform.position.y;
+        initX = transform.position.x;
+        posOffset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        initY+=5*Mathf.Sin(5*Time.deltaTime);
-        initX+=5*Mathf.Sin(5*Time.deltaTime);
+        initY+=(0.01f)*Mathf.Cos(Time.time);
+        initX+=(0.01f)*Mathf.Sin(Time.time);
+
+        posOffset.x = initX;
+        posOffset.y = initY;
+        transform.position = posOffset;
+        transform.Rotate(new Vector3(0f, 0f,(0.01f)*Mathf.Cos(Time.time)), Space.Self);
+
     }
 }
